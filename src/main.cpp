e679b7207fecd245d764b5656c11ad957478e4b8
djordjemaric <djordjegmaric@gmail.com>
2023-04-14 19:15:07 +0200

    dodao skybox i ubacio teksture/objekte u repo

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 unsigned int loadTexture(char const *path);
+unsigned int loadCubemap(vector<std::string> faces);
 
 // settings
 const unsigned int SCR_WIDTH = 1920;
@@ -169,6 +170,7 @@ int main() {
     // -------------------------
     Shader modelShader("resources/shaders/modelShader.vs", "resources/shaders/modelShader.fs");
     Shader planeShader("resources/shaders/planeShader.vs", "resources/shaders/planeShader.fs");
+    Shader skyboxShader("resources/shaders/skyboxShader.vs", "resources/shaders/skyboxShader.fs");
 
     // load models
     // ------------------
@@ -181,17 +183,20 @@ int main() {
     Model brod("resources/objects/ship/SM_Ship12.obj");
     brod.SetShaderTextureNamePrefix("material.");
 
+    Model kofa("resources/objects/bucket/bucket.obj");
+    kofa.SetShaderTextureNamePrefix("material.");
+
 
 
     //Set up water level
     float planeVertices[] = {
-            20.0f, -1.0f,  20.0f,  2.0f, 0.0f,
-            -20.0f, -1.0f,  20.0f,  0.0f, 0.0f,
-            -20.0f, -1.0f, -20.0f,  0.0f, 2.0f,
+            50.0f, -1.0f,  50.0f,  2.0f, 0.0f,
+            -50.0f, -1.0f,  50.0f,  0.0f, 0.0f,
+            -50.0f, -1.0f, -50.0f,  0.0f, 2.0f,
 
-            20.0f, -1.0f,  20.0f,  2.0f, 0.0f,
-            -20.0f, -1.0f, -20.0f,  0.0f, 2.0f,
-            20.0f, -1.0f, -20.0f,  2.0f, 2.0f
+            50.0f, -1.0f,  50.0f,  2.0f, 0.0f,
+            -50.0f, -1.0f, -50.0f,  0.0f, 2.0f,
+            50.0f, -1.0f, -50.0f,  2.0f, 2.0f
     };
 
     unsigned int planeVAO, planeVBO;
@@ -209,6 +214,77 @@ int main() {
     unsigned int waterTexture = loadTexture("resources/textures/water_texture.jpg");
     planeShader.use();
     planeShader.setInt("texture1", 0);
+
+
+
+    float skyboxVertices[] = {
+            // positions
+            -1.0f,  1.0f, -1.0f,
+            -1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            -1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f
+    };
+
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    vector<std::string> faces
+            {
+                    FileSystem::getPath("resources/textures/interstellar_skybox/xpos.png"),
+                    FileSystem::getPath("resources/textures/interstellar_skybox/xneg.png"),
+                    FileSystem::getPath("resources/textures/interstellar_skybox/ypos.png"),
+                    FileSystem::getPath("resources/textures/interstellar_skybox/yneg.png"),
+                    FileSystem::getPath("resources/textures/interstellar_skybox/zpos.png"),
+                    FileSystem::getPath("resources/textures/interstellar_skybox/zneg.png")
+            };
+    stbi_set_flip_vertically_on_load(false);
+    unsigned int cubemapTexture = loadCubemap(faces);
+    skyboxShader.use();
+    skyboxShader.setInt("skybox", 0);
+
 
     //Set up lights
     PointLight& pointLight = programState->pointLight;
@@ -318,6 +394,21 @@ int main() {
         model = glm::scale(model, glm::vec3(0.2f));
         modelShader.setMat4("model", model);
         brod.Draw(modelShader);
+
+
+        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        skyboxShader.use();
+        view = glm::mat4(glm::mat3(programState->camera.GetViewMatrix())); // remove translation from the view matrix
+        skyboxShader.setMat4("view", view);
+        skyboxShader.setMat4("projection", projection);
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // set depth function back to default
+
 
         if (programState->ImGuiEnabled)
             DrawImGui(programState);
@@ -470,6 +561,37 @@ unsigned int loadTexture(char const *path) {
         std::cout << "Texture failed to load at path: " << path << std::endl;
         stbi_image_free(data);
     }
+
+    return textureID;
+}
+
+
+unsigned int loadCubemap(vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
     return textureID;
 }
